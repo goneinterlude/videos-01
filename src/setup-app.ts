1c09258da639fec24130f688fa4bf3a2cca27a4b
Stanislav Sydorenko <goneinterlude@gmail.com>
2026-02-12 15:41:34 +0200

    add testing.router.ts

--- src/setup-app.ts
+++ src/setup-app.ts
@@ -1,6 +1,7 @@
 import express, { Express } from "express";
 import { HttpStatus } from "./core/types/http-statuses";
 import { videosRouter } from "./videos/routers/video.router";
+import { testingRouter} from "./testing/testing.router";
 
 export const setupApp = (app: Express) => {
   app.use(express.json()); // middleware для парсинга JSON в теле запроса
@@ -11,6 +12,8 @@ export const setupApp = (app: Express) => {
   });
 
   app.use("/videos", videosRouter);
+
+  app.use('/testing', testingRouter)
 
   return app;
 };
